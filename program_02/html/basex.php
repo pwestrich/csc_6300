c84b6ae16b5be6831dd551394853f342f5e01216
coursework <coursework@coursework.csc.tntech.edu>
2017-03-04 21:24:31 -0600

    Added the basex query. Still testing to see if it works.

--- program_02/html/basex.php
+++ program_02/html/basex.php
@@ -9,12 +9,12 @@
 		$requestBody = sprintf($requestFormat, $xqueryRequest);
 
 		//build the HTTP request and retuen the body of the result
-		$curl = $curl_init();
+		$curl = curl_init();
 
 		curl_setopt_array($curl, array(
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_POST => 1,
-			CURLOPT_URL => "http://localhost:8984/rest/medsamp2012.xml"
+			CURLOPT_URL => "http://localhost:8984/rest/medsamp2012.xml",
 			CURLOPT_POSTFIELDS => $requestBody
 
 		));
@@ -26,14 +26,47 @@
 	//Function that returns an array of paper types from the basex server
 	function basex_get_paper_types(){
 
+		$xquery = 'for $x at $i in distinct-values(//PublicationType) order by $x return element option { attribute value {$i}, $x }';
+		return basex_query($xquery);
 
 	}
 
 	//Function that will build and execute the xquery for the absex server
-	function basex_build_query_from_request($request){
+	function basex_build_query_from_request($author, $title, $type, $abstract){
+
+		$query = 'for $article in //Article ';
+
+		if (!empty($author)){
+
+			$query = $query . "where \$article/AuthorList/Author/LastName = '$author' ";
+
+		}
+
+		if (!empty($title)){
+
+			$query = $query . "where contains(\$article//Title, '$title') ";
+
+		}
+
+		if (!empty($type)){
+
+			$query = $query . "where \$article//PublicationType = '$type' ";
+
+		}
+
+		if (!empty($abstract)){
+
+			$query = $query . "where contains(\$article//Abstract, '$abstract') ";
+
+		}
+
+		$query = $query . "return element article { \$article//*[self::Author or self::Title or self::AbstractText or self::PublicationType]}";
+
+		return basex_query($query);
 
 
 	}
+
 
 ?>
 
--- program_02/html/index.php
+++ program_02/html/index.php
@@ -1,5 +1,10 @@
 
 <?php
+
+	require "basex.php";
+	
+	var_dump($_POST);
+
 	//paper types for the exist database
 	$EXIST_PAPER_TYPES = array(
 		"0"  => "Journal Article",
@@ -12,7 +17,15 @@
 		"47" => "Conference Proceedings");
 	
 	//paper types for the basex database, will need to be fetched
-	$BASEX_PAPER_TYPES = array();
+	$BASEX_PAPER_TYPES = basex_get_paper_types();
+	
+	$author   = $_POST["authorName"];
+	$title    = $_POST["titleContent"];
+	$type     = $_POST["paperType"];
+	$abstract = $_POST["abstractContent"];
+
+	$basex_results = basex_build_query_from_request($author, $title, $type, $abstract);
+
 
 ?>
 
@@ -22,12 +35,12 @@
     <tr>
       <td><input type="checkbox">Search by Author</input></td>
       <td><p align="right">Author's Name:</p></td>
-      <td><input type="text" name="authorName"/></td>
+      <td><input type="text" name="authorName"/><?php echo $author ?></td>
     </tr>
     <tr>
       <td><input type="checkbox">Search by Title</input></td>
       <td><p align="right">Title contains:</p></td> 
-      <td><input type="text" name="titleContent"/></td>
+      <td><input type="text" name="titleContent"/><?php echo $title ?></td>
     </tr>
     <tr>
       <td><input type="checkbox">Search by Type</input></td>
@@ -36,15 +49,15 @@
         <select name="paperType">
 
 <?php
-	foreach ($EXIST_PAPER_TYPES as $key => $value){
+	/*foreach ($EXIST_PAPER_TYPES as $key => $value){
 
 		echo "<option value='$key'>$value</option>";
 
-	}
+	}*/
 
 	foreach ($BASEX_PAPER_TYPES as $key => $value){
 
-		echo "<option value='$key'>$value</option>";
+		echo $value;
 
 	}
 
@@ -55,7 +68,7 @@
     <tr>
       <td><input type="checkbox">Search by Abstract</input></td>
       <td><p align="right">Abstract contains:</p></td> 
-      <td><input type="text" name="apstractContent"/></td>
+      <td><input type="text" name="abstractContent"/><?php echo $abstract ?></td>
     </tr>
   </table>
   <input type="submit" value="Submit">
@@ -63,5 +76,6 @@
 
 <h2>Results</h2>
 <textarea name="message" rows="10" cols="65">
+<?php echo $basex_results ?>
 </textarea>
 
